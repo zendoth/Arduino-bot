7ef44767fd952f4931f950adf4a708da5de62e05
Mark Nicholas Heah <32152878+zendoth@users.noreply.github.com>
2019-10-16 16:07:49 +0800

    Update final_movement.ino

--- final_movement/final_movement.ino
+++ final_movement/final_movement.ino
@@ -9,7 +9,7 @@ DualVNH5019MotorShield md;
 #define INTERVAL 100
 
 double setRPM = 200.0;   //Desired RPM/120
-double rkp = 1.13; //1.09208      //Kp value for Right Wheel PID
+double rkp = 1.18; //1.09208      //Kp value for Right Wheel PID
 double rki = 0;       //Ki value for Right Wheel PID
 double rkd = 0.15;//0.09;       //Kd value for Right Wheel PID 12
 double lkp = 1.087;  //1.0168;       //Kp value for Left Wheel PID 16.5
@@ -50,77 +50,17 @@ void setup() {
   pinMode(ledPin,OUTPUT);
   pinMode(M1_PIN, INPUT);
   pinMode(M2_PIN, INPUT);
-  attachPinChangeInterrupt(M1_PIN, m1_Interrupt, CHANGE);
-  attachPinChangeInterrupt(M2_PIN, m2_Interrupt, CHANGE);
+  attachPinChangeInterrupt(M1_PIN, m1_Interrupt, HIGH);
+  attachPinChangeInterrupt(M2_PIN, m2_Interrupt, HIGH);
   md.init();
   count=1;
 }
 boolean start=true;
-
-///////////////////////////////////////////PID code////////////////////////////////////////////////
-void digitalPIDleft() {             //Digital PID implementation for Left Wheel
-  double lk1 = lkp + lki + lkd;
-  double lk2 = (-lkp) - (2 * lkd);
-  double lk3 = lkd;
-  lek = (lk1 * lek2) + (lk2 * lek1) + (lk3 * lek0);
-  luk1 = luk0 + lek;
-  luk0 = luk1; 
-  lek0 = lek1;
-  lek1 = lek2;
-}
-
-void realtimePID() {        //PID function to modify speed values using digital PID implementation
-
-  //Perform digital PID for Right Wheel
-  ruk1 = getRPM(1); //read encoder rpm into 'y' (data acquisition)
-  rek2 = getRPM(1)-setRPM; //error between 'set_rpm' and 'y'
-  digitalPIDright();
-  actualR= rpmToPwm(ruk1);
-  actualR*=-1;
-  
-  
-  //Perform digital PID for Left Wheel
-  luk1 = getRPM(2);
-  lek2 = getRPM(2)-setRPM;
-  digitalPIDleft();
-  actualL = rpmToPwm(luk1);//lek;  
-  actualL*=-1;
-}
-
-
-void rotate_counter(int degree){
-  moveStop();
-  delay(110);
-  int total_count = 0;
-  total_count = 8.851*degree-41.4;
-  m1_oldtick = m1_tick;
-  md.setM1Speed(-200);
-  md.setM2Speed(-200);
-  while((m1_tick - m1_oldtick)<total_count);
-  moveStop();
-  delay(50);
-  return;
-}
-void rotate_clock(int degree){
-  moveStop();
-  delay(110);
-  int total_count = 0;
-  total_count = 8.851*degree-41.4;
-  m1_oldtick = m1_tick;
-  md.setM1Speed(200);
-  md.setM2Speed(200);
-  while((m1_tick - m1_oldtick)<total_count);
-  moveStop();
-  delay(50);
-  return;
-}
-///////////////////////////////////////////////////////////////////////////////
 boolean flagf,flagl;
 char choice='Z';
 int ccount;
 int x=0;
 void loop() { 
-
   while (Serial.available()>0){
     int dummy= Serial.peek();
       if(dummy>90||dummy<49){
@@ -132,7 +72,7 @@ void loop() {
    
      switch (choice){
       case 'W'://forward
-      while(count%62!=0){//63
+      while(count%65!=0){//63
         choice=moveForward();
         realtimePID();
         count++;
@@ -140,7 +80,7 @@ void loop() {
       actualR=200;
       actualL=200;
       moveStop();
-      printSensormedian(medianvalue(SharpIR0), medianvalue(SharpIR5),medianvalue(SharpIR1),medianvalue(SharpIR2),medianvalue(SharpIR3),medianLong(SharpIR4));
+      printSensor(modevalue(SharpIR0), modevalue(SharpIR5),modevalue(SharpIR1),modevalue(SharpIR2),modevalue(SharpIR3),modeLong(SharpIR4));
       choice='Z';
       //delay(10);
       count=1;
@@ -148,46 +88,44 @@ void loop() {
       break;
       case 'S'://backwards
         moveBackwards();
-        printSensormedian(medianvalue(SharpIR0), medianvalue(SharpIR5),medianvalue(SharpIR1),medianvalue(SharpIR2),medianvalue(SharpIR3),medianLong(SharpIR4));
+        printSensor(modevalue(SharpIR0), modevalue(SharpIR5),modevalue(SharpIR1),modevalue(SharpIR2),modevalue(SharpIR3),modeLong(SharpIR4));
         break;
       case 'R'://rotate clock
-        //moveClock();
-        rotate_clock(95);
-        printSensormedian(medianvalue(SharpIR0), medianvalue(SharpIR5),medianvalue(SharpIR1),medianvalue(SharpIR2),medianvalue(SharpIR3),medianLong(SharpIR4));
+        rotate_clock(94);
+        printSensor(modevalue(SharpIR0), modevalue(SharpIR5),modevalue(SharpIR1),modevalue(SharpIR2),modevalue(SharpIR3),modeLong(SharpIR4));
       break;                          
       case 'L'://rotate counter
-        //moveCounter();
-        rotate_counter(94);
-        printSensormedian(medianvalue(SharpIR0), medianvalue(SharpIR5),medianvalue(SharpIR1),medianvalue(SharpIR2),medianvalue(SharpIR3),medianLong(SharpIR4)); 
+        rotate_counter(95);
+        printSensor(modevalue(SharpIR0), modevalue(SharpIR5),modevalue(SharpIR1),modevalue(SharpIR2),modevalue(SharpIR3),modeLong(SharpIR4)); 
         break;
       case 'B':
-        printSensormedian(medianvalue(SharpIR0), medianvalue(SharpIR5),medianvalue(SharpIR1),medianvalue(SharpIR2),medianvalue(SharpIR3),medianLong(SharpIR4)); 
+        printSensor(modevalue(SharpIR0), modevalue(SharpIR5),modevalue(SharpIR1),modevalue(SharpIR2),modevalue(SharpIR3),modeLong(SharpIR4)); 
         break;
       case 'C':
         ccount=0;
         while(flagf!=true){
           int l,r;
-          l=medianvalue(SharpIR0);
-          r=medianvalue(SharpIR1);
+          l=modevalue(SharpIR0);
+          r=modevalue(SharpIR1);
           flagf=calibrate(l,r,ccount);
           ccount++;
         }
-        printSensormedian(medianvalue(SharpIR0), medianvalue(SharpIR5),medianvalue(SharpIR1),medianvalue(SharpIR2),medianvalue(SharpIR3),medianLong(SharpIR4));
+        printSensor(modevalue(SharpIR0), modevalue(SharpIR5),modevalue(SharpIR1),modevalue(SharpIR2),modevalue(SharpIR3),modeLong(SharpIR4));
         flagf=false;
         break;
        case 'T':
-       while((medianvalue(SharpIR0)>14)||(medianvalue(SharpIR5)>14)||(medianvalue(SharpIR1)>14)||(medianvalue(SharpIR0)==-1)||(medianvalue(SharpIR5)==-1)||(medianvalue(SharpIR1)==-1)){
+       while((modevalue(SharpIR0)>14)||(modevalue(SharpIR5)>14)||(modevalue(SharpIR1)>14)||(modevalue(SharpIR0)==-1)||(modevalue(SharpIR5)==-1)||(modevalue(SharpIR1)==-1)){
           choice= moveForward();
        }
        moveStop();
        ccount=0;
         while(flagf!=true){
-          flagf=calibrate(medianvalue(SharpIR0),medianvalue(SharpIR1),ccount);
+          flagf=calibrate(modevalue(SharpIR0),modevalue(SharpIR1),ccount);
           ccount++;
         }
         flagf=false;
        break;
-        printSensormedian(medianvalue(SharpIR0), medianvalue(SharpIR5),medianvalue(SharpIR1),medianvalue(SharpIR2),medianvalue(SharpIR3),medianLong(SharpIR4));
+        printSensor(modevalue(SharpIR0), modevalue(SharpIR5),modevalue(SharpIR1),modevalue(SharpIR2),modevalue(SharpIR3),modeLong(SharpIR4));
         break;
       default://else dont move
         moveStop();
